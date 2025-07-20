b676e8de765bdf34b774bc15a1b2677c5d60e39e
Kiet Chau <cgkiet314@gmail.com>
2025-07-21 03:43:17 +0700

    create web movie ticket

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Footer from "./components/Footer";
+import HomePage from "./pages/homePage/HomePage";
+import MoviesPage from "./pages/moviesPage/MoviesPage";
+import MovieDetailPage from "./pages/movieDetailPage/MovieDetailPage";
+import SeatLayout from "./pages/seatLayout/SeatLayout";
+import MyBookingPage from "./pages/myBookingPage/MyBookingPage";
+import FavoritePage from "./pages/favoritePage/FavoritePage";
+import { Toaster } from "react-hot-toast";
+
+const App = () => {
+  // kiểm tra admin
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
+
+  return (
+    <>
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movie:id" element={<MovieDetailPage />} />
+        <Route path="/movie:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookingPage />} />
+        <Route path="/favorites" element={<FavoritePage />} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
+  );
+};
+
+export default App;
