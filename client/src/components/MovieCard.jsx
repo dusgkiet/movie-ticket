f94d682851d08b44b6c7c0572b45f93064ad86d4
Kiet Chau <cgkiet314@gmail.com>
2025-07-25 00:52:18 +0700

    feature[Footer]: Tạo Footer

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -7,7 +7,7 @@ const MovieCard = ({ movie }) => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col justify-between p-3 bg-gray-800 rounded-2xl hover:-translate-y-1 transition duration-300 w-65">
+    <div className="flex flex-col justify-between p-3 bg-gray-800 rounded-2xl hover:-translate-y-1 transition duration-300 w-66">
       <img
         onClick={() => {
           navigate(`/movies/${movie._id}`);
--- client/src/components/TrailersSection.jsx
+++ client/src/components/TrailersSection.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { dummyTrailers } from "../assets/assets";
 import ReactPlayer from "react-player";
 import BlurCircle from "./BlurCircle";
+import { PlayCircleIcon } from "lucide-react";
 
 const TrailersSection = () => {
   const [currentTrailer, setCurrentTrailer] = useState(dummyTrailers[0]);
@@ -21,6 +22,25 @@ const TrailersSection = () => {
           width="960px"
           height="540px"
         />
+      </div>
+
+      <div className="group grid grid-cols-4 gap-4 md:gap-8 mt-8 max-w-3x1 mx-auto">
+        {dummyTrailers.map((trailer) => (
+          <div
+            key={trailer.image}
+            className="relative group-hover:not-hover:opacity-50 hover:-translate-y-1 duration-300 transition max-md:h-60 md:max-h-60 cursor-pointer"
+            onClick={() => setCurrentTrailer(trailer)}>
+            <img
+              src={trailer.img}
+              alt="trailer"
+              className="rounded-1g w-full h-full object-cover brightness-75"
+            />
+            <PlayCircleIcon
+              strokeWidth={1.6}
+              className="absolute top-1/2 left-1/2 w-5 md:w-8 h-5 md:h-12 transform -translate-x-1/2 -translate-y-1/2"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
